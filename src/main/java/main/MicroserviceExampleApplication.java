f1f8fb0bd5a3ea4793e4469da11c8caee6a5474b
p312388041 <312388041@qq.com>
2024-09-20 15:23:15 +0900

    update

--- src/main/java/main/MicroserviceExampleApplication.java
+++ src/main/java/main/MicroserviceExampleApplication.java
@@ -1,23 +1,27 @@
 package main;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class MicroserviceExampleApplication {
     public static void main(String[] args) {
-        String str = "aaa.bbb";
-        System.out.println(str.split("[.]").length);
-    }
-    ThreadLocal<String> threadLocal = ThreadLocal.withInitial(() ->"aa");
-    @RequestMapping("/hello")
-    public String hello() {
-        return "Hello from microservice!";
+        String[] stringArray = {"aaa","bbb"};
+        List<String> list = Arrays.asList(stringArray); 
+        String [] array = list.toArray(String[]::new);
+        // System.out.println(Arrays.toString(array));
+        System.out.println(list);
+        outerloop:
+        for(int i=0;i<10;i++) {
+            System.out.println(i);
+            for(int j=0;j<5;j++) {
+                System.out.println(j);
+                continue outerloop;
+            }
+        }
     }
 }
